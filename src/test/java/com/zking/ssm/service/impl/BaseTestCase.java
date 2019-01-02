4267b44a3c12b253086d77e274b6c6da6e09e303
2018121308 <2018144272@qq.com>
2019-01-02 10:35:28 +0800

    o

---
+++ src/test/java/com/zking/ssm/service/impl/BaseTestCase.java
@@ -0,0 +1,22 @@
+package com.zking.ssm.service.impl;
+
+import com.zking.ssm.model.Book;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+public class BaseTestCase {
+
+    protected Book book;
+
+    @Before
+    public void setUp(){
+        book=new Book();
+    }
+
+
+
+}
